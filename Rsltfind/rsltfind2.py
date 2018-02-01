ce9339f444a60ab1f354e5c0e98a0236c874fd1f
Uditha Kanishka <email.uditha@gmail.com>
2018-02-01 12:00:45 +0530

    Add files via upload

---
+++ Rsltfind/rsltfind2.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from time import sleep
+index = int(input("Enter Index number : "))
+x=0
+browser = webdriver.Chrome('chromedriver/chromedriver.exe')
+browser.get("http://www.doenets.lk/result/alresult.jsf?")
+print ("Opened URL")
+while index <= 6535208 :
+    browser.find_element_by_id('frm:username').clear()
+    print(index)
+    browser.find_element_by_id('frm:username').send_keys(str(index))
+    browser.find_element_by_id('frm:btnSubmit').click()
+    sleep(3)
+    index = index + 1
